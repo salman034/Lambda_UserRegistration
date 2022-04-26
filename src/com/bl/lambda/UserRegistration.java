565b1a908ff11cd762eae6cf44496111befafb05
unknown <you@example.com>
2022-04-26 06:59:05 +0530

    Added: User need to follow Password Rules should have at least 1 Numeric Nmuber

--- src/com/bl/lambda/UserRegistration.java
+++ src/com/bl/lambda/UserRegistration.java
@@ -3,8 +3,8 @@ package com.bl.lambda;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public boolean passwordRule2() {
-        boolean matchesString = Pattern.matches("^[A-Z{1}a-z]{8}$","Mkspasha");
+    public boolean passwordRule3() {
+        boolean matchesString = Pattern.matches("^[A-Z{1}0-9{1}a-z]{8}$", "Sa9amash");
         return matchesString;
     }
 }
--- src/com/bl/lambda/UserRegistrationTest.java
+++ src/com/bl/lambda/UserRegistrationTest.java
@@ -7,9 +7,9 @@ public class UserRegistrationTest {
     private Assertions Assert;
 
     @Test
-    public void givenPasswordAtlestOneUpperCaseWhenShouldReturnTrue() {
+    public void givenPasswordAtlestOneNumericWhenShouldReturnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean userTest = userRegistration.passwordRule2();
+        boolean userTest = userRegistration.passwordRule3();
         System.out.println(userTest);
         Assert.assertTrue(userTest);
     }
